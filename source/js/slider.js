cadf45ebe554ddda61971899dff56880b8b1c4d2
KrapivinAV <min_art@mail.ru>
2020-05-17 14:45:02 +0300

    Устраняет замечания

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,20 +2,20 @@ let sliderImages = document.querySelectorAll('.physique-comparison__image');
 let sliderUnit = document.querySelector('.physique-comparison__toggle');
 let sliderToggles = document.querySelectorAll('.physique-comparison__toggle-item');
 
-console.log (sliderImages);
-console.log (sliderUnit);
-console.log (sliderToggles);
-
 sliderToggles[0].addEventListener('click', function () {
   sliderUnit.classList.remove('physique-comparison__toggle--after');
   sliderUnit.classList.add('physique-comparison__toggle--before');
-  sliderImages[0].classList.remove('visually-hidden');
-  sliderImages[1].classList.add('visually-hidden');
+  sliderImages[0].classList.remove('physique-comparison__image--invisible');
+  sliderImages[0].classList.add('physique-comparison__image--visible');
+  sliderImages[1].classList.remove('physique-comparison__image--visible');
+  sliderImages[1].classList.add('physique-comparison__image--invisible');
 });
 
 sliderToggles[1].addEventListener('click', function () {
   sliderUnit.classList.remove('physique-comparison__toggle--before');
   sliderUnit.classList.add('physique-comparison__toggle--after');
-  sliderImages[1].classList.remove('visually-hidden');
-  sliderImages[0].classList.add('visually-hidden');
+  sliderImages[1].classList.remove('physique-comparison__image--invisible');
+  sliderImages[1].classList.add('physique-comparison__image--visible');
+  sliderImages[0].classList.remove('physique-comparison__image--visible');
+  sliderImages[0].classList.add('physique-comparison__image--invisible');
 });
